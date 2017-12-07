029f36dd73e254ac82152d348f7d1086aadc05ab
up730418 <up730418@myport.ac.uk>
2017-12-07 00:16:17 +0000

    demo page

--- api/index.js
+++ api/index.js
@@ -3,3 +3,6 @@ const api = express.Router();
 module.exports = api;
 
 api.use('/js', require('./js'));
+
+api.get('/', (req, res) => {res.send('an api endpoint')});
+
--- api/js/index.js
+++ api/js/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const js = express.Router();
 module.exports = js;
 
-
+js.get('/find', (req, res) => {
+		res.send('Find is realy just syntactical sugar like many things in js you could use a for loop but this is just cleaner. code demo insues')
+	});
